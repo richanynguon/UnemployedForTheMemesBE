6638ea0579a2bd1097ecc81b481b2050db44861b
Richany <alina.nguon@gmail.com>
2021-01-13 18:59:10 -0500

    revert main app added favicon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Resource, Api
 import os
 
 
@@ -9,13 +8,16 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 db = SQLAlchemy(app)
 
-api = Api(app)
+@app.route('/', methods=['GET'])
+def index():
+    msg = "running"
+    print('I am running index')
+    return jsonify(msg)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-api.add_resource(HelloWorld, '/')
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run(debug=True)
